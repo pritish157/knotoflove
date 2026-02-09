89fabf575a1678c0c58f012ea64fc9b90a10ae1d
pritish157 <pritishpanda157@gmail.com>
2026-02-09 23:54:45 +0530

    fix: connect MongoDB using mongoose and dotenv

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const app = require("./src/app");
+const connectdb = require("./src/config/db")
+
 
 const PORT = process.env.PORT || 5000;
-
+connectdb()
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
